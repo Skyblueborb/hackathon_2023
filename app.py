4c188e3206a04ae0f4cab5a52cff3e182052dc8d
Skyblueborb <tomaszborbely0710@gmail.com>
2023-04-13 16:48:24 +0200

    flask: converter logic and link css style

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 
 NOMINALS = Counter()
 CALC = 0
+YEAR = 1970
 
 
 @app.route("/")
@@ -19,22 +20,25 @@ def index():
 def converter():
     global NOMINALS
     global CALC
+    global YEAR
 
     if request.method == "GET":
-        nominals, calc = NOMINALS, CALC
-        NOMINALS, CALC = Counter(), 0
-        return render_template("converter.html", nominals=nominals, calc=calc)
+        nominals, calc, year = NOMINALS, CALC, YEAR
+        NOMINALS, CALC, YEAR = Counter(), 0, 1970
+        return render_template(
+            "converter.html", nominals=nominals, calc=calc, year=year
+        )
 
     if request.method == "POST":
         sum = request.form.get("sum")
         if "-" in sum:
             return redirect("/converter")
-        year = request.form.get("year")
+        YEAR = request.form.get("year")
         try:
-            fact = factor(int(year))
+            fact = factor(int(YEAR))
+            CALC = calculate(numCleaner(sum), fact)
         except (NotImplementedError, ValueError):
             return redirect("/converter")
-        CALC = calculate(float(sum), fact)
         NOMINALS = getNominals(CALC)
 
         return redirect("/converter")
