7032c0ef6810781e2ce5ef0469d8f4c8a0ffe98c
Skyblueborb <tomaszborbely0710@gmail.com>
2023-04-13 15:00:33 +0200

    curreny: implement the flask requests for currency converter

--- app.py
+++ app.py
@@ -1,10 +1,40 @@
-from flask import Flask, render_template, request
+from math import exp
+from typing import Dict
+from flask import Flask, redirect, render_template, request
+from currency import *
 
 app = Flask(__name__)
+
+
+NOMINALS = Counter()
+CALC = 0
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
-@app.route("/converter")
+
+
+@app.route("/converter", methods=["GET", "POST"])
 def converter():
-    return render_template("converter.html")
+    global NOMINALS
+    global CALC
+
+    if request.method == "GET":
+        nominals, calc = NOMINALS, CALC
+        NOMINALS, CALC = Counter(), 0
+        return render_template("converter.html", nominals=nominals, calc=calc)
+
+    if request.method == "POST":
+        sum = request.form.get("sum")
+        if "-" in sum:
+            return redirect("/converter")
+        year = request.form.get("year")
+        try:
+            fact = factor(int(year))
+        except (NotImplementedError, ValueError):
+            return redirect("/converter")
+        CALC = calculate(float(sum), fact)
+        NOMINALS = getNominals(CALC)
+
+        return redirect("/converter")
--- currency.py
+++ currency.py
@@ -2,42 +2,54 @@ import requests
 from bs4 import BeautifulSoup
 from collections import Counter
 from math import floor
+
 DOLLAR_COURSE_FACTOR = 100.0
 CURRENT_PAY = 76153.80
 ZUS = "https://www.zus.pl/baza-wiedzy/skladki-wskazniki-odsetki/wskazniki/przecietne-wynagrodzenie-w-latach"
+
+
 def csv_reader(path: str) -> dict[float, str]:
     if not isinstance(path, str):
         raise TypeError
-    with open(path, 'r') as file:
+    with open(path, "r") as file:
         rawData = []
         for i in file:
             rawData.append(i[:-1])
         data = [i.split(",") for i in rawData]
-        return {numCleaner(i[0]) : i[1] for i in data}
-def numCleaner(number: str) -> float: # czyści wprowadzoną wartość z dziwnych formatacji
+        return {numCleaner(i[0]): i[1] for i in data}
+
+
+def numCleaner(
+    number: str,
+) -> float:  # czyści wprowadzoną wartość z dziwnych formatacji
     output = number.replace(" ", "", -1).replace(",", ".", -1)
     try:
         return float(output)
     except Exception:
         raise ValueError
-def factor(year: int) -> float: # średnia pensja dla roku [year]
+
+
+def factor(year: int) -> float:  # średnia pensja dla roku [year]
     if year not in range(1960, 1980):
-        raise NotImplementedError# robimy tylko dla lat 1960-79
-    soup = BeautifulSoup(requests.get(ZUS).text, 'html.parser')
-    return numCleaner((soup.select(".zus-cms>table>tbody>tr>td")[1::3][year-1950]).text)
-calculate = lambda amount, fact: (-1 if amount < 0 else round((amount/fact)*CURRENT_PAY, 2))# (-1): error
+        raise NotImplementedError  # robimy tylko dla lat 1960-79
+    soup = BeautifulSoup(requests.get(ZUS).text, "html.parser")
+    return numCleaner(
+        (soup.select(".zus-cms>table>tbody>tr>td")[1::3][year - 1950]).text
+    )
+
+
+calculate = lambda amount, fact: (
+    -1 if amount < 0 else round((amount / fact) * CURRENT_PAY, 2)
+)  # (-1): error
+
+
 def getNominals(money: float) -> Counter[str]:
     noms = csv_reader("nominals.csv")
     count = Counter()
-    curMoney = money
+    curMoney = money + 0.00001
     for val, nom in reversed(noms.items()):
         temp = floor(curMoney // val)
         if temp:
             count[nom] += temp
-            curMoney -= val*temp
+            curMoney -= val * temp
     return count
-print(getNominals(3.50))
-
-
-
-
